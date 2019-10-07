af9e884cadf9fe30b64340299a0cd1c28f3605bd
leoalonso99 <leonardoalonso07@hotmail.com>
2019-10-06 22:04:27 -0300

    logicadeprogramacao

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace imcexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = new string[3];
+            double[] altura = new double[3];
+            double[] peso = new double[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                
+            Console.WriteLine("Digite o seu nome:");
+            nome[i]= Console.ReadLine();
+
+            Console.WriteLine("Digite a sua altura:");
+            altura[i] = double.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Digite o seu peso:");
+            peso[i] = double.Parse(Console.ReadLine());  
+
+            Console.WriteLine($"{nome[i]} seu IMC é: {peso[i]/(altura[i]*altura[i])}");
+            }
+
+            
+        }
+    }
+}
